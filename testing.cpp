7e81d2bb405628f6af9eac6786906ccf24101323
Himank <oculust123@gmail.com>
2024-12-11 06:19:16 +0530

    power of 2?

--- testing.cpp
+++ testing.cpp
@@ -1,41 +1,34 @@
-//rev an integer <simple math operations>
 #include <iostream>
 #include <math.h>
 using namespace std;
 int main() {
-    int length=0,n,rev=0;
-    cin>>n;
-    int k=n;
-    while(k!=0){
-    	k=k/10;
-    	length++;
-    }
-    while(n!=0){
-    	rev+=(n%10)*(pow(10,length-1));
-    	n=n/10;
-    	length--;
-    	//cout<<rev<<endl;
-    }
+	int n,count=0;
+	long long counter=1;
+	cin>> n;
+	//using log and floor function
+	// if(floor(log2(n))==log2(n)){
+	// 	cout<<"power of 2";
+	// }
+ //    else{
+ //    	cout<<"not power of 2";
+ //    }
+	//using logic:maths;
+	while(n>=counter){
+		if(n==counter){
+			
+			counter=-1;
+			break;
+		}
+		counter=counter*2;
+		
+
+	}
+	if(counter==-1){
+		cout<<"number is a power of 2"<<endl;
+	}
+	else{
+		cout<<"not a power of 2"<<endl;
+	}
+	
     return 0;
 }
-
-//Leetcode Submission
-// class Solution {
-// public:
-//     //soln=0;
-//     int reverse(int x) {
-//         int length=0,rev=0;
-//         int k=x;
-//         while(k!=0){
-//     	    k=k/10;
-//     	    length++;
-//         }
-//         while(x!=0){
-//     	    rev+=(x%10)*(pow(10,length-1));
-//     	    x=x/10;
-//     	    length--;
-//     	//cout<<rev<<endl;
-//         }
-//         return rev;
-//     }
-// };
