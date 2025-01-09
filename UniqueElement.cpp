6143cb64fcfd5f1a1d29c9a9ad795e6209c1a294
Himank <oculust123@gmail.com>
2025-01-09 17:11:48 +0530

    intersection2arr

--- UniqueElement.cpp
+++ UniqueElement.cpp
@@ -1,16 +1,26 @@
-//leetcode: Single number, find unique element in an array;
+//intersection of an array
 #include <iostream>
 using namespace std;
 int main() {
-    int a[10],n,xorsum=0;
-    cin>>n;
+    int a[10],b[10],t[10],n,m,start=0;
+    cin>>n>>m;
     for(int i=0;i<n;i++){
-    	cin>>a[i];
+        cin>>a[i];
+    }
+    for(int i=0;i<m;i++){
+        cin>>b[i];
     }
     for(int i=0;i<n;i++){
-    	xorsum = xorsum ^ a[i];
+        for(int j=0;j<m;j++){
+            if(a[i]==b[j]){
+                t[start]=a[i];
+                start++;
+                
+            }
+        }
     }
-    cout<<xorsum;
-
+    for(int i=0;i<start;i++){
+        cout<<t[i]<<endl;
+    }
     return 0;
 }
