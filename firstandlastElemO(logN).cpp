3a6d6c496a42051f0fe518268e2abfc505a58510
Himank <oculust123@gmail.com>
2025-01-12 07:31:23 +0530

    firstandLast O(logN) time

--- firstandlastElemO(logN).cpp
+++ firstandlastElemO(logN).cpp
@@ -53,3 +53,51 @@ int main() {
 
     return 0;
 }
+//leetCode solution
+class Solution {
+public:
+    int firstOccurance(vector<int> &a,int n,int key){
+    int start=0,ans=-1;
+    int end=n-1;
+    int mid=start+(end-start)/2;
+    while(start<=end){
+        if(a[mid]==key){
+            ans=mid;
+            end=mid-1;
+        }
+        else if(a[mid]<key){
+            start=mid+1;
+        }
+        else {
+            end=mid-1;
+        }
+        mid=start+(end-start)/2;
+    }
+    return ans;
+    }
+    int lastOccurance(vector<int> &a,int n,int key){
+        int start=0,ans=-1;
+        int end=n-1;
+        int mid=start+(end-start)/2;
+        while(start<=end){
+            if(a[mid]==key){
+                ans=mid;
+                start=mid+1;
+            }
+            else if(a[mid]<key){
+                start=mid+1;
+            }
+            else {
+                end=mid-1;
+            }
+            mid=start+(end-start)/2;
+        }
+    return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> v;
+        v.push_back(firstOccurance(nums,nums.size(),target));
+        v.push_back(lastOccurance(nums,nums.size(),target));
+        return v;
+    }
+};
